6d238aab3663dc4040afbd1fb59c5716d12c6cd7
djurayevdavron <djurayevdavron7@gmail.com>
2026-07-23 00:01:19 +0500

    Upgrade to Prisma 7.9 configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +16,7 @@ async function bootstrap() {
     }),
   );
 
+  // Swagger Configuration (Swagger konfiguratsiyasi)
   const config = new DocumentBuilder()
     .setTitle('NestJS Prisma API')
     .setDescription(
@@ -27,11 +30,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3000;
-
-  await app.listen(port);
-
-  console.log(`Application is running on port ${port}`);
+  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
